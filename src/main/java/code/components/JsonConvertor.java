42996cf07bdbcbed926b3ee389b6600a3c81e458
panesar1810 <panesar.pb08@gmail.com>
2020-03-22 02:09:34 -0400

    #4

---
+++ src/main/java/code/components/JsonConvertor.java
@@ -0,0 +1,22 @@
+package code.components;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@Component @Lazy
+public class JsonConvertor {
+ 
+	private Gson GSON = new Gson();
+	
+	public Function<String, Map<String, Object>> toHashMap = data -> 
+		GSON.fromJson(data, new TypeToken<HashMap<String, Object>>()
+		{}.getType());
+		
+}
